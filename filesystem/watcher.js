f07b6630ade58b2549c15aadc25cfd70b0ae4da6
farooqam <farooq_am@hotmail.com>
2018-08-04 22:59:30 -0700

    Spawning a child process.

--- filesystem/watcher.js
+++ filesystem/watcher.js
@@ -1,11 +1,18 @@
 "use strict";
 
 const fs = require("fs");
+const spawn = require("child_process").spawn;
 const target = process.argv[2];
 
 if (!target) {
     throw Error('Specify a file to watch.');
 }
 
-fs.watch(target, () => console.log(`File ${target} has changed.`));
+fs.watch(
+    target, 
+    () => {
+        const ls = spawn("ls", ["-l", "-h", target]);
+        ls.stdout.pipe(process.stdout);
+    });
+    
 console.log(`Watching ${target} for changes.`);
